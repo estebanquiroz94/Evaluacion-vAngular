4b23688812070a7ff81119a7a6cbed7662628296
estebanquiroz94 <soportedevsteam@gmail.com>
2019-10-19 19:18:49 -0500

    Servicio para validacion de Login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {LoginService} from './login/login.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
