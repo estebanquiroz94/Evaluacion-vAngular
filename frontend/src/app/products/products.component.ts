e7a61faac256e768b59ed66b876ad55b81b2b63f
estebanquiroz94 <jesteban.quiroz@gmail.com>
2019-10-30 23:10:01 -0500

    Ajustes para visualización de imagenes

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -12,14 +12,13 @@ export class ProductsComponent {
   @Input() product : any;
   initialUrl: string = 'http://localhost:3008/api/';
 
-  products = [{nameProduct: 'Aguacate', image: '', price: '20', unitsAvailable: 5},
-  {nameProduct: 'Canela', image: '', price: '2', unitsAvailable: 320},
-  {nameProduct: 'Ajo', image: '', price: '5', unitsAvailable: 20},
-  {nameProduct: 'Almendras', image: '', price: '35', unitsAvailable: 800},
-  {nameProduct: 'Arándanos', image: '', price: '30', unitsAvailable: 500},
-  {nameProduct: 'Brócoli', image: '', price: '80', unitsAvailable: 450},
-  {nameProduct: 'Guayaba', image: '', price: '10', unitsAvailable: 280},
-  {nameProduct: 'Carenepe', image: '', price: '1000', unitsAvailable: 1}
+  products = [{nameProduct: 'Aguacate', image: '../../assets/img/canela.jpg', price: '20', unitsAvailable: 5},
+  {nameProduct: 'Canela', image: '../../assets/img/tomate.jpg', price: '2', unitsAvailable: 320},
+  {nameProduct: 'Ajo', image: '../../assets/img/ajo.jpg', price: '5', unitsAvailable: 20},
+  {nameProduct: 'Almendras', image: '../../assets/img/almendras.jpg', price: '35', unitsAvailable: 800},
+  {nameProduct: 'Arándanos', image: '../../assets/img/arandanos.jpg', price: '30', unitsAvailable: 500},
+  {nameProduct: 'Brócoli', image: '../../assets/img/brocoli.jpg', price: '80', unitsAvailable: 450},
+  {nameProduct: 'Guayaba', image: '../../assets/img/guayaba.jpg', price: '10', unitsAvailable: 280}
 ]
 
   constructor(private _httpClient: HttpClient) 
