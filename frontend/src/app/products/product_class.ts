d608a7d8972d494a5a0593f6757c74da9d04c84c
estebanquiroz94 <jesteban.quiroz@gmail.com>
2019-11-04 12:28:17 -0500

    Se realiza ajuste de clase PRODUCT

---
+++ frontend/src/app/products/product_class.ts
@@ -0,0 +1,7 @@
+class Product {
+    _id: String;
+    nameProduct: String;
+    image: String;
+    price: String;
+    unitsAvaliable: String
+}
--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -1,5 +1,4 @@
 ;import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import { ProductService} from './product.service';
 
 
@@ -15,18 +14,10 @@ export class ProductsComponent {
   products2 :any[]
   products :Product[] = []
 
-  // products = [{nameProduct: 'Aguacate', image: '../../assets/img/canela.jpg', price: '20', unitsAvailable: 5},
-  // {nameProduct: 'Canela', image: '../../assets/img/tomate.jpg', price: '2', unitsAvailable: 320},
-  // {nameProduct: 'Ajo', image: '../../assets/img/ajo.jpg', price: '5', unitsAvailable: 20},
-  // {nameProduct: 'Almendras', image: '../../assets/img/almendras.jpg', price: '35', unitsAvailable: 800},
-  // {nameProduct: 'Arándanos', image: '../../assets/img/arandanos.jpg', price: '30', unitsAvailable: 500},
-  // {nameProduct: 'Brócoli', image: '../../assets/img/brocoli.png', price: '80', unitsAvailable: 450},
-  // {nameProduct: 'Guayaba', image: '../../assets/img/guayaba.png', price: '10', unitsAvailable: 280}
-//]
-
   constructor(private _productService: ProductService) 
   {    
 
+    //Load an show all products registers
     this._productService.loadAllProducts()
     .subscribe(
       (data: Product[]) =>{
@@ -39,10 +30,3 @@ export class ProductsComponent {
   }
 }
 
-class Product {
-    _id: String;
-    nameProduct: String;
-    image: String;
-    price: String;
-    unitsAvaliable: String
-}
