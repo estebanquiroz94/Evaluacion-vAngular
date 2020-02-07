ca29c46d7e2631fdebe35fe16f2063ae236c74ed
estebanquiroz94 <jesteban.quiroz@gmail.com>
2020-02-07 09:23:23 -0500

    Ajuste en peticion de productos

--- frontend/src/app/products/product.service.ts
+++ frontend/src/app/products/product.service.ts
@@ -42,9 +42,6 @@ export class ProductService {
     
     producto.user = user;
     
-    var result = JSON.stringify(producto);
-    console.log(result);
-    
     //Start petition for API Backend
     this.data = this._httpClient.post(finalUrl, producto)
     .map((response: Response) => response)
